cc3f448c85d8fae6e39b4cfecefdcab4953a9bea
Olga Furmanowska <olgafurmanowska@gmail.com>
2018-05-27 10:46:21 +0200

    Fix bugs in app.js file

--- app.js
+++ app.js
@@ -56,16 +56,18 @@ componentWillMount: function() {
 },
 
   render: function() {
-    return React.createElement('div',
-      React.createElement('button', {onClick: this.increment, id: 'sumButton'}, 'Add 1'),
-        React.createElement('span', {}, 'counter ' + this.state.counter),
-      React.createElement('button', {onClick: this.decrement},'Rest 1'),
-        React.createElement('span', {}, 'counter ' + this.state.counter),
-      React.createElement('button', {onClick: this.incrementTen}, 'Add 10'),
-        React.createElement('span', {}, 'counter ' + this.state.counter),
-      React.createElement('button', {onClick: this.decrementTen}, 'Rest 10'),
-        React.createElement('span', {}, 'counter ' + this.state.counter),
-      );
+    return (
+      React.createElement('div', {className: 'template_buttons'},
+          React.createElement('h2', {}, 'Counter' ),
+          React.createElement('div', {className: 'counter_buttons'},
+            React.createElement('button', {onClick: this.increment, id: 'sumButton'}, 'Add 1'),
+            React.createElement('button', {onClick: this.decrement},'Rest 1'),
+            React.createElement('button', {onClick: this.incrementTen}, 'Add 10'),
+            React.createElement('button', {onClick: this.decrementTen}, 'Rest 10')
+          ),
+          React.createElement('span', {}, 'Counter ' + this.state.counter),
+      )
+    )
   },
   componentDidMount: function() {
     console.log('W tej metodzie możemy wywołać wcześniej napisaną funkcję, która zeruje licznik, kiedy ten dojdzie do pewnej wartości. Przyklad poniżej');
@@ -88,7 +90,7 @@ componentWillMount: function() {
     console.log('Za pomocą tej metody możemy dokonać manipulacji na drzewie DOM, na przykład dodać eventListener na zdarzenie onClick jednym z buttonów, który będzie uruchamiał funkcję do sumowania stanu wszystkich liczników na przykład');
       var sumButton = document.getElementById('sumButton');
       var sumCounters =  function() {
-        return this.state.counter * 4;
+        return counter1.state.counter + counter2.state.counter + counter3.state.counter;
       };
       sumButton.addEventListener("click", sumCounters);
   },
@@ -98,6 +100,9 @@ componentWillMount: function() {
   }
 
 });
+var counter1 = React.createElement(Counter);
+var counter2 = React.createElement(Counter);
+var counter3 = React.createElement(Counter);
 
-var element = React.createElement(Counter);
+var element = React.createElement('div', {}, counter1, counter2, counter3);
 ReactDOM.render(element, document.getElementById('app'));
